f5f0a54be7faadc31b7231c716504e903f23f6a4
darknebula05 <camms205@aol.com>
2024-02-27 13:48:16 -0500

    Add nix devShell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = fenix.packages.${system}.default.toolchain;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            toolchain
+            pkgs.rust-analyzer-nightly
+          ];
+        };
+      }
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,10 @@ impl Cpu6502 {
             | (self.z as u8) << 1
             | (self.c as u8)
     }
+    fn update_n_and_z(&mut self) {
+        self.n = self.a & 0x80 > 0;
+        self.z = self.a == 0;
+    }
 
     fn abs(&mut self) -> u8 {
         self.operand = self.fetch_word();
@@ -154,12 +158,20 @@ impl Cpu6502 {
         let fetched = self.read_byte(self.operand);
         let temp = self.a as u16 + fetched as u16 + self.c as u16;
         self.c = temp > 255;
-        self.z = temp & 0x00ff == 0;
         self.v = ((!(self.a as u16 ^ fetched as u16) & (self.a as u16 ^ temp)) & 0x0080) != 0;
         self.a = temp as u8;
+        self.update_n_and_z();
     }
-    fn and(&mut self) {}
-    fn asl(&mut self) {}
+    fn and(&mut self) {
+        self.a &= self.operand as u8;
+        self.update_n_and_z();
+    }
+    fn asl(&mut self) {
+        let temp = (self.a as u16) << 1;
+        self.c = temp > 255;
+        self.a = temp as u8;
+        self.update_n_and_z();
+    }
     fn bcc(&mut self) {}
     fn bcs(&mut self) {}
     fn beq(&mut self) {}
@@ -214,62 +226,62 @@ impl Cpu6502 {
     fn txs(&mut self) {}
     fn tya(&mut self) {}
 
-    pub const LOOKUP: [(&str, fn(&mut Cpu6502) -> u8, &str); 256] = instructions!(
-        adc: (imm, 0x69, 2), (abs, 0x6d, 4), (abs_x, 0x7d, 4+p), (abs_y, 0x79, 4+p), (zp, 0x65, 3), (zp_x, 0x75, 4), (zp_x_indirect, 0x61, 6), (zp_y_indirect, 0x71, 5+p);
-        and: (imm, 0x29, 2), (abs, 0x2d, 4), (abs_x, 0x3d, 4+p), (abs_y, 0x39, 4+p), (zp, 0x25, 3), (zp_x, 0x35, 4), (zp_x_indirect, 0x21, 6), (zp_y_indirect, 0x31, 5+p);
-        asl: (acc, 0x0a, 2), (abs, 0x0e, 6), (abs_x, 0x1e, 7), (zp, 0x06, 5), (zp_x, 0x16, 6);
-        bcc: (rel, 0x90, 2+t+p);
-        bcs: (rel, 0xb0, 2+t+p);
-        beq: (rel, 0xf0, 2+t+p);
-        bit: (abs, 0x2c, 4), (zp, 0x24, 3);
-        bmi: (rel, 0x30, 2+t+p);
-        bne: (rel, 0xd0, 2+t+p);
-        bpl: (rel, 0x10, 2+t+p);
-        brk: (imp, 0x00, 7);
-        bvc: (rel, 0x50, 2+t+p);
-        bvs: (rel, 0x70, 2+t+p);
-        clc: (imp, 0x18, 2);
-        cld: (imp, 0xd8, 2);
-        cli: (imp, 0x58, 2);
-        clv: (imp, 0xb8, 2);
-        cmp: (imm, 0xc9, 2), (abs, 0xcd, 4), (abs_x, 0xdd, 4+p), (abs_y, 0xd9, 4+p), (zp, 0xc5, 3), (zp_x, 0xd5, 4), (zp_x_indirect, 0xc1, 6), (zp_y_indirect, 0xd1, 5+p);
-        cpx: (imm, 0xe0, 2), (abs, 0xec, 4), (zp, 0xe4, 3);
-        cpy: (imm, 0xc0, 2), (abs, 0xcc, 4), (zp, 0xc4, 3);
-        dec: (abs, 0xce, 6), (abs_x, 0xde, 7), (zp, 0xc6, 5), (zp_x, 0xd6, 6);
-        dex: (imp, 0xca, 2);
-        dey: (imp, 0x88, 2);
-        eor: (imm, 0x49, 2), (abs, 0x4d, 4), (abs_x, 0x5d, 4+p), (abs_y, 0x59, 4+p), (zp, 0x45, 3), (zp_x, 0x55, 4), (zp_x_indirect, 0x41, 6), (zp_y_indirect, 0x51, 5+p);
-        inc: (abs, 0xee, 6), (abs_x, 0xfe, 7), (zp, 0xe6, 5), (zp_x, 0xf6, 6);
-        inx: (imp, 0xe8, 2);
-        iny: (imp, 0xc8, 2);
-        jmp: (abs, 0x4c, 3), (abs_indirect, 0x6c, 5);
-        jsr: (abs, 0x20, 6);
-        lda: (imm, 0xa9, 2), (abs, 0xad, 4), (abs_x, 0xbd, 4+p), (abs_y, 0xb9, 4+p), (zp, 0xa5, 3), (zp_x, 0xb5, 4), (zp_x_indirect, 0xa1, 6), (zp_y_indirect, 0xb1, 5+p);
-        ldx: (imm, 0xa2, 2), (abs, 0xae, 4), (abs_y, 0xbe, 4+p), (zp, 0xa6, 3), (zp_y, 0xb6, 4);
-        ldy: (imm, 0xa0, 2), (abs, 0xac, 4), (abs_x, 0xbc, 4+p), (zp, 0xa4, 3), (zp_x, 0xb4, 4);
-        lsr: (acc, 0x4a, 2), (abs, 0x4e, 6), (abs_x, 0x5e, 7), (zp, 0x46, 5), (zp_x, 0x56, 6);
-        nop: (imp, 0xea, 2);
-        ora: (imm, 0x09, 2), (abs, 0x0d, 4), (abs_x, 0x1d, 4+p), (abs_y, 0x19, 4+p), (zp, 0x05, 3), (zp_x, 0x15, 4), (zp_x_indirect, 0x01, 6), (zp_y_indirect, 0x11, 5+p);
-        pha: (imp, 0x48, 3);
-        php: (imp, 0x08, 3);
-        pla: (imp, 0x68, 4);
-        plp: (imp, 0x28, 4);
-        rol: (acc, 0x2a, 2), (abs, 0x2e, 6), (abs_x, 0x3e, 7), (zp, 0x26, 5), (zp_x, 0x36, 6);
-        ror: (acc, 0x6a, 2), (abs, 0x6e, 6), (abs_x, 0x7e, 7), (zp, 0x66, 5), (zp_x, 0x76, 6);
-        rti: (imp, 0x40, 6);
-        rts: (imp, 0x60, 6);
-        sbc: (imm, 0xe9, 2), (abs, 0xed, 4), (abs_x, 0xfd, 4+p), (abs_y, 0xf9, 4+p), (zp, 0xe5, 3), (zp_x, 0xf5, 4), (zp_x_indirect, 0xe1, 6), (zp_y_indirect, 0xf1, 5+p);
-        sec: (imp, 0x38, 2);
-        sed: (imp, 0xf8, 2);
-        sei: (imp, 0x78, 2);
-        sta: (abs, 0x8d, 4), (abs_x, 0x9d, 5), (abs_y, 0x99, 5), (zp, 0x85, 3), (zp_x, 0x95, 4), (zp_x_indirect, 0x81, 6), (zp_y_indirect, 0x91, 6);
-        stx: (abs, 0x8e, 4), (zp, 0x86, 3), (zp_y, 0x96, 4);
-        sty: (abs, 0x8c, 4), (zp, 0x84, 3), (zp_x, 0x94, 4);
-        tax: (imp, 0xaa, 2);
-        tay: (imp, 0xa8, 2);
-        tsx: (imp, 0xba, 2);
-        txa: (imp, 0x8a, 2);
-        txs: (imp, 0x9a, 2);
-        tya: (imp, 0x98, 2);
-    );
+    // pub const LOOKUP: [(&str, fn(&mut Cpu6502) -> u8, &str); 256] = instructions!(
+    //     adc: (imm, 0x69, 2), (abs, 0x6d, 4), (abs_x, 0x7d, 4+p), (abs_y, 0x79, 4+p), (zp, 0x65, 3), (zp_x, 0x75, 4), (zp_x_indirect, 0x61, 6), (zp_y_indirect, 0x71, 5+p);
+    //     and: (imm, 0x29, 2), (abs, 0x2d, 4), (abs_x, 0x3d, 4+p), (abs_y, 0x39, 4+p), (zp, 0x25, 3), (zp_x, 0x35, 4), (zp_x_indirect, 0x21, 6), (zp_y_indirect, 0x31, 5+p);
+    //     asl: (acc, 0x0a, 2), (abs, 0x0e, 6), (abs_x, 0x1e, 7), (zp, 0x06, 5), (zp_x, 0x16, 6);
+    //     bcc: (rel, 0x90, 2+t+p);
+    //     bcs: (rel, 0xb0, 2+t+p);
+    //     beq: (rel, 0xf0, 2+t+p);
+    //     bit: (abs, 0x2c, 4), (zp, 0x24, 3);
+    //     bmi: (rel, 0x30, 2+t+p);
+    //     bne: (rel, 0xd0, 2+t+p);
+    //     bpl: (rel, 0x10, 2+t+p);
+    //     brk: (imp, 0x00, 7);
+    //     bvc: (rel, 0x50, 2+t+p);
+    //     bvs: (rel, 0x70, 2+t+p);
+    //     clc: (imp, 0x18, 2);
+    //     cld: (imp, 0xd8, 2);
+    //     cli: (imp, 0x58, 2);
+    //     clv: (imp, 0xb8, 2);
+    //     cmp: (imm, 0xc9, 2), (abs, 0xcd, 4), (abs_x, 0xdd, 4+p), (abs_y, 0xd9, 4+p), (zp, 0xc5, 3), (zp_x, 0xd5, 4), (zp_x_indirect, 0xc1, 6), (zp_y_indirect, 0xd1, 5+p);
+    //     cpx: (imm, 0xe0, 2), (abs, 0xec, 4), (zp, 0xe4, 3);
+    //     cpy: (imm, 0xc0, 2), (abs, 0xcc, 4), (zp, 0xc4, 3);
+    //     dec: (abs, 0xce, 6), (abs_x, 0xde, 7), (zp, 0xc6, 5), (zp_x, 0xd6, 6);
+    //     dex: (imp, 0xca, 2);
+    //     dey: (imp, 0x88, 2);
+    //     eor: (imm, 0x49, 2), (abs, 0x4d, 4), (abs_x, 0x5d, 4+p), (abs_y, 0x59, 4+p), (zp, 0x45, 3), (zp_x, 0x55, 4), (zp_x_indirect, 0x41, 6), (zp_y_indirect, 0x51, 5+p);
+    //     inc: (abs, 0xee, 6), (abs_x, 0xfe, 7), (zp, 0xe6, 5), (zp_x, 0xf6, 6);
+    //     inx: (imp, 0xe8, 2);
+    //     iny: (imp, 0xc8, 2);
+    //     jmp: (abs, 0x4c, 3), (abs_indirect, 0x6c, 5);
+    //     jsr: (abs, 0x20, 6);
+    //     lda: (imm, 0xa9, 2), (abs, 0xad, 4), (abs_x, 0xbd, 4+p), (abs_y, 0xb9, 4+p), (zp, 0xa5, 3), (zp_x, 0xb5, 4), (zp_x_indirect, 0xa1, 6), (zp_y_indirect, 0xb1, 5+p);
+    //     ldx: (imm, 0xa2, 2), (abs, 0xae, 4), (abs_y, 0xbe, 4+p), (zp, 0xa6, 3), (zp_y, 0xb6, 4);
+    //     ldy: (imm, 0xa0, 2), (abs, 0xac, 4), (abs_x, 0xbc, 4+p), (zp, 0xa4, 3), (zp_x, 0xb4, 4);
+    //     lsr: (acc, 0x4a, 2), (abs, 0x4e, 6), (abs_x, 0x5e, 7), (zp, 0x46, 5), (zp_x, 0x56, 6);
+    //     nop: (imp, 0xea, 2);
+    //     ora: (imm, 0x09, 2), (abs, 0x0d, 4), (abs_x, 0x1d, 4+p), (abs_y, 0x19, 4+p), (zp, 0x05, 3), (zp_x, 0x15, 4), (zp_x_indirect, 0x01, 6), (zp_y_indirect, 0x11, 5+p);
+    //     pha: (imp, 0x48, 3);
+    //     php: (imp, 0x08, 3);
+    //     pla: (imp, 0x68, 4);
+    //     plp: (imp, 0x28, 4);
+    //     rol: (acc, 0x2a, 2), (abs, 0x2e, 6), (abs_x, 0x3e, 7), (zp, 0x26, 5), (zp_x, 0x36, 6);
+    //     ror: (acc, 0x6a, 2), (abs, 0x6e, 6), (abs_x, 0x7e, 7), (zp, 0x66, 5), (zp_x, 0x76, 6);
+    //     rti: (imp, 0x40, 6);
+    //     rts: (imp, 0x60, 6);
+    //     sbc: (imm, 0xe9, 2), (abs, 0xed, 4), (abs_x, 0xfd, 4+p), (abs_y, 0xf9, 4+p), (zp, 0xe5, 3), (zp_x, 0xf5, 4), (zp_x_indirect, 0xe1, 6), (zp_y_indirect, 0xf1, 5+p);
+    //     sec: (imp, 0x38, 2);
+    //     sed: (imp, 0xf8, 2);
+    //     sei: (imp, 0x78, 2);
+    //     sta: (abs, 0x8d, 4), (abs_x, 0x9d, 5), (abs_y, 0x99, 5), (zp, 0x85, 3), (zp_x, 0x95, 4), (zp_x_indirect, 0x81, 6), (zp_y_indirect, 0x91, 6);
+    //     stx: (abs, 0x8e, 4), (zp, 0x86, 3), (zp_y, 0x96, 4);
+    //     sty: (abs, 0x8c, 4), (zp, 0x84, 3), (zp_x, 0x94, 4);
+    //     tax: (imp, 0xaa, 2);
+    //     tay: (imp, 0xa8, 2);
+    //     tsx: (imp, 0xba, 2);
+    //     txa: (imp, 0x8a, 2);
+    //     txs: (imp, 0x9a, 2);
+    //     tya: (imp, 0x98, 2);
+    // );
 }
